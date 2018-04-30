9178cecceb6c35ad1017259a90bf5a421f82c974
pandeod <onkardpande@gmail.com>
2018-04-30 17:39:50 +0530

    Fetch data from drive excel added

--- app/src/main/java/com/example/onkarpande/mp_project/Activity/SearchFragment.java
+++ app/src/main/java/com/example/onkarpande/mp_project/Activity/SearchFragment.java
@@ -1,20 +1,32 @@
 package com.example.onkarpande.mp_project.Activity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.onkarpande.mp_project.Adapter.MenuAdapter;
 import com.example.onkarpande.mp_project.Entity.ItemMenu;
+import com.example.onkarpande.mp_project.Entity.JSONParser;
+import com.example.onkarpande.mp_project.Entity.MenuList;
 import com.example.onkarpande.mp_project.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,18 +61,11 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         itemMenus=new ArrayList<>();
         adapter=new MenuAdapter(itemMenus,getContext());
 
-
-
-        itemMenus.add(new ItemMenu("1","First Item","$1","https://vedh.000webhostapp.com/images/dow.png"));
-        itemMenus.add(new ItemMenu("2","Second Item","$2","https://vedh.000webhostapp.com/images/dow.png"));
-        itemMenus.add(new ItemMenu("3","Third Item","$3","https://vedh.000webhostapp.com/images/dow.png"));
-        itemMenus.add(new ItemMenu("4","Four Item","$4","https://vedh.000webhostapp.com/images/dow.png"));
-        itemMenus.add(new ItemMenu("5","Five Item","$5","https://vedh.000webhostapp.com/images/dow.png"));
-        itemMenus.add(new ItemMenu("6","Six Item","$6","https://vedh.000webhostapp.com/images/dow.png"));
-
        // itemMenus.add(new ItemMenu("1","First Item","$123",""));
 
         recyclerView.setAdapter(adapter);
+
+        new GetDataTask().execute();
 
         EditText searchText=root.findViewById(R.id.search_bar);
         searchText.addTextChangedListener(new TextWatcher() {
@@ -106,5 +111,121 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         }
         adapter=new MenuAdapter(filterList,getContext());
         recyclerView.setAdapter(adapter);
+    }
+
+
+    /**
+     * Creating Get Data Task for Getting Data From Web
+     */
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog dialog;
+        int jIndex;
+        int x;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+
+            x=itemMenus.size();
+
+            if(x==0)
+                jIndex=0;
+            else
+                jIndex=x;
+
+            dialog = new ProgressDialog(getContext());
+            dialog.setTitle("Syncing...");
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONParser.getDataFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if(jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = null;
+                        try {
+                            array = jsonObject.getJSONArray(MenuList.KEY_CONTACTS);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        /**
+                         * Check Length of Array...
+                         */
+
+
+                        int lenArray = array.length();
+                        if(lenArray > 0) {
+                            for( ; jIndex < lenArray; jIndex++) {
+
+                                /**
+                                 * Creating Every time New Object
+                                 * and
+                                 * Adding into List
+                                 */
+
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+                                String menu = innerObject.getString(MenuList.KEY_MENU);
+                                String price = innerObject.getString(MenuList.KEY_PRICE);
+                                String url = innerObject.getString(MenuList.KEY_URL);
+                                String id=innerObject.getString(MenuList.KEY_ID);
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                itemMenus.add(new ItemMenu(id,menu,price,url));
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            }
+            catch (JSONException je) {
+                Log.i(JSONParser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(itemMenus.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Toast.makeText(getContext(),"No data Found",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/onkarpande/mp_project/Entity/CartList.java
+++
@@ -1,9 +0,0 @@
-package com.example.onkarpande.mp_project.Entity;
-
-public class CartList {
-
-    public static final String KEY_SHEET="Sheet1";
-    public static final String KEY_ID="Id";
-    public static final String KEY_MENU="Menu";
-    public static final String KEY_PRICE="Price";
-}
